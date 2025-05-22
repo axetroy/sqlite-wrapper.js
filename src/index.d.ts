c9103948d96575d64aed67e6928f93c900b49578
Axetroy <axetroy.dev@gmail.com>
2025-05-22 15:37:17 +0800

    更新 SQLiteWrapper 的 exec 和 query 方法，支持可选的参数数组

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,9 +1,9 @@
 export declare class SQLiteWrapper {
 	constructor(exePath: string, dbPath: string);
 
-	exec(sql: string): Promise<void>;
+	exec(sql: string, params?: any[]): Promise<void>;
 
-	query(sql: string): Promise<any[]>;
+	query(sql: string, params?: any[]): Promise<any[]>;
 
 	close(): Promise<void>;
 }
