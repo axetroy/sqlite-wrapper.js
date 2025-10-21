397c7de07456d0c1566644913e8251f1962d33a4
Axetroy <axetroy.dev@gmail.com>
2025-10-21 14:12:42 +0800

    fix: 添加 escapeValue 和 interpolateSQL 函数的声明

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,4 +1,15 @@
-export * from "./utils.js";
+/**
+ * Escape a single SQL value.
+ * @param value
+ */
+export declare function escapeValue(value: any): string;
+
+/**
+ * Escape SQL values in the given SQL string with the provided parameters.
+ * @param sql
+ * @param params
+ */
+export declare function interpolateSQL(sql: string, params: any[]): string;
 
 export interface Logger {
 	log(...messages: unknown[]): void;
